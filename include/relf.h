e62e51fc38071650dcfdbd9f81ad20b1b9c5b03f
James Fraser <wulfgar.pro@gmail.com>
2025-07-31 16:46:26 +1000

    feat: finish parsing of ELF64 header

--- include/relf.h
+++ include/relf.h
@@ -2,7 +2,8 @@
 
 #include <stdint.h>
 
-#define EI_NIDENT 16 /* Number of bytes in e_ident. */
+// Number of bytes in e_ident.
+#define EI_NIDENT 16
 
 typedef enum {
   RELF_CLASS_NONE = 0,
@@ -11,16 +12,16 @@ typedef enum {
 } Relf_E_Class;
 
 enum {
-  EI_MAG0 = 0,    /* Magic number: 0x7F */
-  EI_MAG1 = 1,    /* Magic number: 0x45 (E) */
-  EI_MAG2 = 2,    /* Magic number: 0x4C (L) */
-  EI_MAG3 = 3,    /* Magic number: 0x46 (F) */
-  EI_CLASS = 4,   /* 1 or 2 to signify 32- or 64-bit format. */
-  EI_DATA = 5,    /* 1 or 2 to signify little or big endianness. */
-  EI_VERSION = 6, /* 1 for original and current version of ELF. */
-  EI_OSABI = 7,   /* Target OS ABI; 0x00 for System V */
-  EI_ABIVERSION = 8,
-  EI_PAD = 9 /* Unused */
+  EI_MAG0 = 0,       // Magic number: 0x7F
+  EI_MAG1 = 1,       // Magic number: 0x45 (E)
+  EI_MAG2 = 2,       // Magic number: 0x4C (L)
+  EI_MAG3 = 3,       // Magic number: 0x46 (F)
+  EI_CLASS = 4,      // 1 or 2 to signify 32- or 64-bit format.
+  EI_DATA = 5,       // 1 or 2 to signify little or big endianness.
+  EI_VERSION = 6,    // 1 for original and current version of ELF.
+  EI_OSABI = 7,      // Target OS ABI; 0x00 for System V
+  EI_ABIVERSION = 8, //
+  EI_PAD = 9         //
 };
 
 typedef struct {
@@ -32,7 +33,17 @@ typedef struct {
   uint16_t e_type;
   uint16_t e_machine;
   uint32_t e_version;
-  // TODO: Add more fields.
+  uint64_t e_entry;
+  uint64_t e_phoff;
+  uint64_t e_shoff;
+  uint32_t e_flags;
+  uint16_t e_ehsize;
+  uint16_t e_phentsize;
+  uint16_t e_phnum;
+  uint16_t e_shentsize;
+  uint16_t e_shnum;
+  uint16_t e_shstrndx;
+
 } Relf_Elf64_Ehdr;
 
 typedef struct {
@@ -43,6 +54,6 @@ typedef struct {
   } ehdr;
 } Relf_Elf_Ehdr;
 
-void relf_print_elf64_header(const Relf_Elf64_Ehdr *ehdr);
 int relf_is_valid_elf_magic(const Relf_E_Ident *e_ident);
 Relf_E_Class relf_determine_elf_class(const Relf_E_Ident *e_ident);
+void relf_print_elf64_header(const Relf_Elf64_Ehdr *ehdr);
--- src/relf.c
+++ src/relf.c
@@ -2,21 +2,6 @@
 #include <stdint.h>
 #include <stdio.h>
 
-void relf_print_elf64_header(const Relf_Elf64_Ehdr *ehdr) {
-  const uint8_t *ident = ehdr->e_ident.e_ident;
-  printf("e_ident_ei_mag: 0x%02x 0x%02x 0x%02x 0x%02x\n", ident[EI_MAG0],
-         ident[EI_MAG1], ident[EI_MAG2], ident[EI_MAG3]);
-  printf("e_ident_ei_class: 0x%02x\n", ident[EI_CLASS]);
-  printf("e_ident_ei_data: 0x%02x\n", ident[EI_DATA]);
-  printf("e_ident_ei_version: 0x%02x\n", ident[EI_VERSION]);
-  printf("e_ident_ei_osabi: 0x%02x\n", ident[EI_OSABI]);
-  printf("e_ident_ei_abiversion: 0x%02x\n", ident[EI_ABIVERSION]);
-
-  printf("e_type: 0x%04x\n", ehdr->e_type);
-  printf("e_machine: 0x%04x\n", ehdr->e_machine);
-  printf("e_version: 0x%08x\n", ehdr->e_version);
-}
-
 int relf_is_valid_elf_magic(const Relf_E_Ident *e_ident) {
   const uint8_t *ident = e_ident->e_ident;
   return ident[EI_MAG0] == 0x7F && ident[EI_MAG1] == 'E' &&
@@ -34,4 +19,29 @@ Relf_E_Class relf_determine_elf_class(const Relf_E_Ident *e_ident) {
   default:
     return RELF_CLASS_NONE;
   }
+}
+
+void relf_print_elf64_header(const Relf_Elf64_Ehdr *ehdr) {
+  const uint8_t *ident = ehdr->e_ident.e_ident;
+  printf("e_ident_ei_mag: 0x%02x 0x%02x 0x%02x 0x%02x\n", ident[EI_MAG0],
+         ident[EI_MAG1], ident[EI_MAG2], ident[EI_MAG3]);
+  printf("e_ident_ei_class: 0x%02x\n", ident[EI_CLASS]);
+  printf("e_ident_ei_data: 0x%02x\n", ident[EI_DATA]);
+  printf("e_ident_ei_version: 0x%02x\n", ident[EI_VERSION]);
+  printf("e_ident_ei_osabi: 0x%02x\n", ident[EI_OSABI]);
+  printf("e_ident_ei_abiversion: 0x%02x\n", ident[EI_ABIVERSION]);
+
+  printf("e_type: 0x%04x\n", ehdr->e_type);
+  printf("e_machine: 0x%04x\n", ehdr->e_machine);
+  printf("e_version: 0x%08x\n", ehdr->e_version);
+  printf("e_entry: 0x%016lx\n", ehdr->e_entry);
+  printf("e_phoff: 0x%016lx\n", ehdr->e_phoff);
+  printf("e_shoff: 0x%016lx\n", ehdr->e_shoff);
+  printf("e_flags: 0x%08x\n", ehdr->e_flags);
+  printf("e_ehsize: 0x%04x\n", ehdr->e_ehsize);
+  printf("e_phentsize: 0x%04x\n", ehdr->e_phentsize);
+  printf("e_phnum: 0x%04x\n", ehdr->e_phnum);
+  printf("e_shentsize: 0x%04x\n", ehdr->e_shentsize);
+  printf("e_shnum: 0x%04x\n", ehdr->e_shnum);
+  printf("e_shstrndx: 0x%04x\n", ehdr->e_shstrndx);
 }
